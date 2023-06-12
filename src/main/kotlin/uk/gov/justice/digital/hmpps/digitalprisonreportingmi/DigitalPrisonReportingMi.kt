470bbefd664e2e7bd40a10153b3e84e180ababfd
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-06-12 10:01:26 +0100

    [DPR-383] Stubbed count

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingmi
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication()
+@SpringBootApplication
 class DigitalPrisonReportingMi
 
 fun main(args: Array<String>) {
