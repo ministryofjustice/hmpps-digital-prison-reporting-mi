2567fa38e7cfedb14ebf42fc3cf8825812107218
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2023-06-15 10:24:44 +0100

    [DPR-495] added a test for the empty table case

--- src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/integration/EstablishmentsIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/integration/EstablishmentsIntegrationTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.integration
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.digitalprisonreportingmi.data.Establishment
@@ -10,6 +11,11 @@ class EstablishmentsIntegrationTest : IntegrationTestBase() {
 
   @Autowired
   lateinit var establishmentRepository: EstablishmentRepository
+
+  @BeforeEach
+  fun setup(){
+    establishmentRepository.deleteAll()
+  }
 
   @Test
   fun `Establishments count returns expected value`() {
@@ -25,5 +31,16 @@ class EstablishmentsIntegrationTest : IntegrationTestBase() {
       .isOk
       .expectBody()
       .jsonPath("count").isEqualTo(expectedCount)
+  }
+
+  @Test
+  fun `Establishments count returns 200 with count zero when the table is empty`() {
+    webTestClient.get()
+      .uri("/establishments/count")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("count").isEqualTo(0)
   }
 }
