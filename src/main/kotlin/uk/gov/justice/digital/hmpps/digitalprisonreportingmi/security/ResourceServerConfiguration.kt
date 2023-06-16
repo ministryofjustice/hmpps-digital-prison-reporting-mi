70593d38091ab65722fc2f73cc819540b0d317b3
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-06-16 13:07:17 +0100

    [DPR-548] Fix JWT decoding

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/security/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/security/ResourceServerConfiguration.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.security
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -11,10 +10,6 @@ import org.springframework.security.web.SecurityFilterChain
 @Profile("!test")
 @Configuration
 class ResourceServerConfiguration {
-
-  @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-  private lateinit var jwkUri: String
-
   @Bean
   @Throws(Exception::class)
   fun filterChain(http: HttpSecurity): SecurityFilterChain? {
@@ -32,7 +27,6 @@ class ResourceServerConfiguration {
       }
       oauth2ResourceServer { jwt {
         jwtAuthenticationConverter = AuthAwareTokenConverter()
-        jwkSetUri = jwkUri
       } }
     }
     return http.build()
