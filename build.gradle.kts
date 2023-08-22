c916dae669b9d0993af4ade6c43c5ed19ef5413d
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-08-22 12:04:52 +0100

    [DPR-439] Add coverage to PR

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   kotlin("jvm") version "1.8.21"
   kotlin("plugin.spring") version "1.8.21"
   kotlin("plugin.jpa") version "1.8.21"
+  id("jacoco")
   id("org.barfuin.gradle.jacocolog") version "3.1.0"
 }
 
@@ -40,10 +41,20 @@ tasks {
     }
   }
 }
+
 repositories {
   mavenCentral()
   maven("https://s3.amazonaws.com/redshift-maven-repository/release")
 }
+
 kotlin {
   jvmToolchain(19)
+}
+
+tasks.test {
+  finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
 }
