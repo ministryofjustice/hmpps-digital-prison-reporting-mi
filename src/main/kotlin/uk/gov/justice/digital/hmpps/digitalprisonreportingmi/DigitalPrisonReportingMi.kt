656af363d91928b3f20d9d11e9d8cedc73367f72
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2023-10-11 11:10:35 +0100

    DPR2-69 deleted data model since this is imported from the library

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@ComponentScan("uk.gov.justice.digital.hmpps.digitalprisonreportingmi","uk.gov.justice.digital.hmpps.digitalprisonreportinglib")
+@ComponentScan("uk.gov.justice.digital.hmpps.digitalprisonreportingmi", "uk.gov.justice.digital.hmpps.digitalprisonreportinglib")
 class DigitalPrisonReportingMi
 
 fun main(args: Array<String>) {
