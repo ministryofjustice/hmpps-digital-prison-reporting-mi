423e26270ad02e1dc41c48e5ffee8d8075c4b227
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-03-07 09:34:20 +0000

    Dpr2 269 app insights custom dim (#139)
    * DPR2-269 added username and activeCaseloadId to app insights
    
    * DPR2-269 fix token parsing issue with issued and expiration dates
    
    * DPR2-269 deploy to Dev
    
    * DPR2-269 try to see if username is tracked
    
    * DPR2-269 attempt to use SecurityContextHolder
    
    * DPR2-269 uncomment pipeline main filter

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/AppInsightsConfig.kt
@@ -0,0 +1,34 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
+
+import io.opentelemetry.api.trace.Span
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
+
+@Configuration
+class AppInsightsConfig(private val clientTrackingInterceptor: ClientTrackingInterceptor) : WebMvcConfigurer {
+  override fun addInterceptors(registry: InterceptorRegistry) {
+    log.info("Adding application insights client tracking interceptor")
+    registry.addInterceptor(clientTrackingInterceptor).addPathPatterns("/**")
+  }
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+}
+
+@Configuration
+class ClientTrackingInterceptor : HandlerInterceptor {
+  override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+    val token = SecurityContextHolder.getContext().authentication as DprAuthAwareAuthenticationToken
+    val user = token.jwt.subject
+    Span.current().setAttribute("username", user) // username in customDimensions
+    Span.current().setAttribute("activeCaseLoadId", token.getCaseLoads().first().toString())
+    return true
+  }
+}
