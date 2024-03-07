2d9e3dc9760dd7669a00b1212367a7ec813eec7b
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2024-03-07 14:33:07 +0000

    [DPR2-457] User caseload endpoint (#140)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/integration/UserApiIntegrationTest.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.integration
+
+import org.junit.jupiter.api.Test
+import org.springframework.web.util.UriBuilder
+
+class UserApiIntegrationTest : IntegrationTestBase() {
+
+  @Test
+  fun `User's active caseloads are returned successfully`() {
+    webTestClient.get()
+      .uri { uriBuilder: UriBuilder ->
+        uriBuilder
+          .path("/user/caseload/active")
+          .build()
+      }
+      .headers(setAuthorisation(roles = listOf(authorisedRole)))
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectBody()
+      .json("[\"LWSTMC\"]")
+  }
+}
