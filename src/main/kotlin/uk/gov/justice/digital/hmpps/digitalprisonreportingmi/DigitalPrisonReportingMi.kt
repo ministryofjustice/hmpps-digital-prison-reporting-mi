55992685bc048e5823203ec5c0c58416a8ebba33
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-03-20 16:56:03 +0000

    DPR2-503 remove commented lines

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
@@ -3,8 +3,6 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingmi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-// @ConfigurationPropertiesScan
-// @ComponentScan("uk.gov.justice.digital.hmpps.digitalprisonreportingmi", "uk.gov.justice.digital.hmpps.digitalprisonreportinglib")
 @SpringBootApplication
 class DigitalPrisonReportingMi
 
