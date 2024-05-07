3eebac89121fbac33057870dc8e8226823566d93
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-05-07 21:58:29 +0100

    DPR2-660 try with secretAccessKey and accessKeyId

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/RedshiftDataApiConf.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/RedshiftDataApiConf.kt
@@ -1,15 +1,22 @@
 package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient
 
 @Configuration
-class RedshiftDataApiConf {
+class RedshiftDataApiConf(
+  @Value("\${dpr.lib.redshiftdataapi.secretaccesskey}") private val secretAccessKey: String,
+  @Value("\${dpr.lib.redshiftdataapi.accesskeyid}") private val accessKeyId: String,
+) {
   @Bean
   fun redshiftDataClient(): RedshiftDataClient {
     return RedshiftDataClient.builder()
+      .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
       .region(Region.EU_WEST_2)
       .build()
   }
