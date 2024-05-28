fa9669fd518e6ae6bdef549134a36a2b65fe6759
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-05-28 16:25:20 +0100

    DPR 2-758 added DB user to status response to debug s3 access denies

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/controller/TestExternalTableController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/controller/TestExternalTableController.kt
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient
+import software.amazon.awssdk.services.redshiftdata.model.DescribeStatementRequest
 import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementRequest
 import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementResponse
 import java.util.UUID
@@ -60,5 +62,43 @@ class TestExternalTableController(
       )
   }
 
+  @Hidden
+  @GetMapping("/test/async/{statementId}/status")
+  @Operation(
+    description = "Test external table creation status.",
+    security = [ SecurityRequirement(name = "bearer-jwt") ],
+  )
+  fun getTestStatementStatus(
+    @PathVariable("statementId") statementId: String,
+    authentication: Authentication,
+  ): ResponseEntity<StatusResponse> {
+    val statementRequest = DescribeStatementRequest.builder()
+      .id(statementId)
+      .build()
+    val describeStatementResponse = redshiftDataClient.describeStatement(statementRequest)
+    return ResponseEntity
+      .status(HttpStatus.OK)
+      .body(
+        StatusResponse(
+          status = describeStatementResponse.statusAsString(),
+          duration = describeStatementResponse.duration(),
+          queryString = describeStatementResponse.queryString(),
+          resultRows = describeStatementResponse.resultRows(),
+          resultSize = describeStatementResponse.resultSize(),
+          dbUser = describeStatementResponse.dbUser(),
+          error = describeStatementResponse.error(),
+        ),
+      )
+  }
+
   data class Response(val tableId: String, val statementId: String)
+  data class StatusResponse(
+    val status: String,
+    val duration: Long,
+    val queryString: String,
+    val resultRows: Long,
+    val resultSize: Long?,
+    val dbUser: String,
+    val error: String? = null,
+  )
 }
