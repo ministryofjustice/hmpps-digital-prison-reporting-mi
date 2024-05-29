cbd4528c67390cf21fb705871f5439a66eea32a4
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-05-29 11:07:25 +0100

    DPR 2-758 added test endpoint to get results

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/controller/TestExternalTableController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/controller/TestExternalTableController.kt
@@ -3,16 +3,19 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.controller
 import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import jakarta.validation.constraints.Min
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.core.Authentication
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient
-import software.amazon.awssdk.services.redshiftdata.model.DescribeStatementRequest
 import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementRequest
 import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementResponse
 import java.util.UUID
@@ -25,6 +28,9 @@ class TestExternalTableController(
   val redshiftDataClient: RedshiftDataClient,
   val executeStatementRequestBuilder: ExecuteStatementRequest.Builder,
 ) {
+
+  @Autowired
+  lateinit var jdbcTemplate: JdbcTemplate
 
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
@@ -63,42 +69,23 @@ class TestExternalTableController(
   }
 
   @Hidden
-  @GetMapping("/test/async/{statementId}/status")
+  @GetMapping("/test/async/result/{tableId}/")
   @Operation(
-    description = "Test external table creation status.",
+    description = "Test external table results.",
     security = [ SecurityRequirement(name = "bearer-jwt") ],
   )
   fun getTestStatementStatus(
-    @PathVariable("statementId") statementId: String,
+    @PathVariable("tableId") tableId: String,
+    @RequestParam(defaultValue = "1")
+    @Min(1)
+    selectedPage: Long,
+    @RequestParam(defaultValue = "10")
+    @Min(1)
+    pageSize: Long,
     authentication: Authentication,
-  ): ResponseEntity<StatusResponse> {
-    val statementRequest = DescribeStatementRequest.builder()
-      .id(statementId)
-      .build()
-    val describeStatementResponse = redshiftDataClient.describeStatement(statementRequest)
-    return ResponseEntity
-      .status(HttpStatus.OK)
-      .body(
-        StatusResponse(
-          status = describeStatementResponse.statusAsString(),
-          duration = describeStatementResponse.duration(),
-          queryString = describeStatementResponse.queryString(),
-          resultRows = describeStatementResponse.resultRows(),
-          resultSize = describeStatementResponse.resultSize(),
-          dbUser = describeStatementResponse.dbUser(),
-          error = describeStatementResponse.error(),
-        ),
-      )
+  ): List<Map<String, Any?>> {
+    return jdbcTemplate.queryForList("SELECT * FROM $tableId limit $pageSize OFFSET ($selectedPage - 1) * $pageSize;")
   }
 
   data class Response(val tableId: String, val statementId: String)
-  data class StatusResponse(
-    val status: String,
-    val duration: Long,
-    val queryString: String,
-    val resultRows: Long,
-    val resultSize: Long?,
-    val dbUser: String,
-    val error: String? = null,
-  )
 }
