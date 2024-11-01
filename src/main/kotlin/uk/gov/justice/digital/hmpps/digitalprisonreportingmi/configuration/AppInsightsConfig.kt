22e487c05d9d9e53121485c3a19b7566087edfbd
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-11-01 11:23:41 +0000

    DPR2-1334 capture product name, variant name and page (#362)
    * DPR2-1334 capture product name, variant name and page
    
    * DPR2-1334 error message
    
    * DPR2-1334 linting
    
    * DPR2-1334 fix failing tests with proper handling of static mocks
    
    ---------
    
    Co-authored-by: Stu Pegg <110236995+stalwartstu@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/AppInsightsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/AppInsightsConfig.kt
@@ -9,7 +9,9 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.ReportDefinitionController
 import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.ReportDefinitionService
 
 @Configuration
 class AppInsightsConfig(private val clientTrackingInterceptor: ClientTrackingInterceptor) : WebMvcConfigurer {
@@ -26,14 +28,46 @@ class AppInsightsConfig(private val clientTrackingInterceptor: ClientTrackingInt
 }
 
 @Configuration
-class ClientTrackingInterceptor : HandlerInterceptor {
+class ClientTrackingInterceptor(val reportDefinitionService: ReportDefinitionService) : HandlerInterceptor {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
     if (SecurityContextHolder.getContext().authentication is DprAuthAwareAuthenticationToken) {
       val token = SecurityContextHolder.getContext().authentication as DprAuthAwareAuthenticationToken
       val user = token.jwt.subject
       Span.current().setAttribute("username", user) // username in customDimensions
       Span.current().setAttribute("activeCaseLoadId", token.getCaseLoads().first().toString())
+      captureDpdAndPageDetails(request, token)
     }
     return true
   }
+
+  private fun captureDpdAndPageDetails(
+    request: HttpServletRequest,
+    token: DprAuthAwareAuthenticationToken,
+  ) {
+    try {
+      val regex = Regex("""/reports/([^/]+)/(?!metrics(/|$))([^/]+)""")
+      val resultMatched = regex.find(request.requestURI)
+      val productId = resultMatched?.let { resultMatched.groupValues[1] }
+      val reportVariantId = resultMatched?.let { resultMatched.groupValues[3] }
+      if (matchExists(productId, reportVariantId)) {
+        val dataProductDefinitionsPath = request.parameterMap["dataProductDefinitionsPath"]?.get(0)
+          ?: ReportDefinitionController.DATA_PRODUCT_DEFINITIONS_PATH_EXAMPLE
+        val pageNumber = request.parameterMap["selectedPage"]?.get(0)
+        val definition = reportDefinitionService.getDefinition(productId!!, reportVariantId!!, token, dataProductDefinitionsPath)
+        Span.current().setAttribute("product", definition.name) // product name in customDimensions
+        Span.current().setAttribute("reportName", definition.variant.name) // variant name in customDimensions
+        pageNumber?.let { Span.current().setAttribute("page", pageNumber) } // page number in customDimensions
+      }
+    } catch (e: Exception) {
+      log.error("Failed to log product name, variant name or selected page to App Insights: {}", e.message)
+    }
+  }
+
+  private fun matchExists(productId: String?, reportVariantId: String?) =
+    !productId.isNullOrBlank() && !reportVariantId.isNullOrBlank()
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/ClientTrackingInterceptorTest.kt
@@ -0,0 +1,151 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
+
+import io.opentelemetry.api.trace.Span
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.mockito.MockedStatic
+import org.mockito.Mockito
+import org.mockito.Mockito.anyString
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.clearInvocations
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.whenever
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletResponse
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.ReportDefinitionController.Companion.DATA_PRODUCT_DEFINITIONS_PATH_EXAMPLE
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.model.SingleVariantReportDefinition
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.controller.model.VariantDefinition
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.security.DprAuthAwareAuthenticationToken
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.service.ReportDefinitionService
+import uk.gov.justice.digital.hmpps.digitalprisonreportingmi.security.FakeCaseloadProvider
+import java.time.Instant
+
+class ClientTrackingInterceptorTest {
+  companion object {
+    val token = DprAuthAwareAuthenticationToken(
+      jwt = Jwt(
+        "abc",
+        Instant.now(),
+        Instant.now().plusMillis(100),
+        mapOf("Authentication" to "Bearer token"),
+        mapOf("sub" to "userA"),
+      ),
+      aPrincipal = "principal",
+      authorities = emptyList(),
+      caseloadProvider = FakeCaseloadProvider(),
+    )
+    private val staticSecurityContextHolderMockUtil: MockedStatic<SecurityContextHolder> = Mockito.mockStatic(SecurityContextHolder::class.java)
+    val span: Span = mock()
+
+    @JvmStatic
+    @BeforeAll
+    fun setUp() {
+      val securityContext = mock<SecurityContext>()
+      staticSecurityContextHolderMockUtil.`when`<SecurityContext> { SecurityContextHolder.getContext() }.thenReturn(securityContext)
+      whenever(securityContext.authentication).thenReturn(token)
+      val staticSpanMockUtil: MockedStatic<Span> = Mockito.mockStatic(Span::class.java)
+      staticSpanMockUtil.`when`<Span> { Span.current() }.thenReturn(span)
+    }
+
+    @JvmStatic
+    @AfterAll
+    fun tearDown() {
+      staticSecurityContextHolderMockUtil.close()
+    }
+  }
+
+  @BeforeEach
+  fun setUpEach() {
+    clearInvocations(span)
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+    strings = [
+      "/reports/external-movements/last-month",
+      "/reports/external-movements/last-month/fieldid",
+      "/reports/external-movements/last-month/count",
+      "/async/reports/external-movements/last-month",
+      "/reports/external-movements/last-month/tables/tableId/result",
+      "/reports/external-movements/last-month/tables/tableId/result/summary/summaryId",
+      "/reports/external-movements/last-month/statements/statementId/status",
+      "/reports/external-movements/last-month/statements/statementId",
+    ],
+  )
+  fun `should call app insights with product name, variant name and selected page`(uri: String) {
+    val reportDefinitionService = mock<ReportDefinitionService>()
+    val clientTrackingInterceptor = ClientTrackingInterceptor(reportDefinitionService)
+    val request = MockHttpServletRequest("GET", uri)
+    request.setParameter("selectedPage", "1")
+    val response = MockHttpServletResponse()
+    val productId = "external-movements"
+    val variantId = "last-month"
+    val productName = "External Movements"
+    val variantName = "Last Month"
+    val definition = SingleVariantReportDefinition(
+      id = productId,
+      name = productName,
+      variant = VariantDefinition(
+        id = variantId,
+        name = variantName,
+        resourceName = "a",
+      ),
+    )
+    whenever(
+      reportDefinitionService.getDefinition(productId, variantId, token, DATA_PRODUCT_DEFINITIONS_PATH_EXAMPLE),
+    ).thenReturn(definition)
+
+    clientTrackingInterceptor.preHandle(request, response, mock())
+
+    verify(reportDefinitionService, times(1))
+      .getDefinition(productId, variantId, token, DATA_PRODUCT_DEFINITIONS_PATH_EXAMPLE)
+    verify(span, times(1))
+      .setAttribute("username", "userA")
+    verify(span, times(1))
+      .setAttribute("activeCaseLoadId", "LWSTMC")
+    verify(span, times(1))
+      .setAttribute("product", productName)
+    verify(span, times(1))
+      .setAttribute("reportName", variantName)
+    verify(span, times(1))
+      .setAttribute("page", "1")
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+    strings = [
+      "/reports/external-movements/metrics/metricId",
+      "/report/tables/tableId/count",
+      "/definitions/external-movements/last-month",
+    ],
+  )
+  fun `should not call app insights with product name, variant name and selected page for non matching uris`(uri: String) {
+    val reportDefinitionService = mock<ReportDefinitionService>()
+    val clientTrackingInterceptor = ClientTrackingInterceptor(reportDefinitionService)
+    val request = MockHttpServletRequest("GET", uri)
+    val response = MockHttpServletResponse()
+
+    clientTrackingInterceptor.preHandle(request, response, mock())
+
+    verifyNoInteractions(reportDefinitionService)
+    verify(span, times(1))
+      .setAttribute("username", "userA")
+    verify(span, times(1))
+      .setAttribute("activeCaseLoadId", "LWSTMC")
+    verify(span, times(0))
+      .setAttribute(eq("product"), anyString())
+    verify(span, times(0))
+      .setAttribute(eq("reportName"), anyString())
+    verify(span, times(0))
+      .setAttribute(eq("page"), anyString())
+  }
+}
