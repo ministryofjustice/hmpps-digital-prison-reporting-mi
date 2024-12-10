83fec3b15e762aee7ee24ebb9938ca9899652606
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-12-10 20:42:48 +0000

    DPR2-1514 pass credentials to DynamoDB client

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/DynamoDbConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/DynamoDbConfig.kt
@@ -2,9 +2,15 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
 
 import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
 import aws.smithy.kotlin.runtime.auth.awscredentials.CredentialsProvider
+import aws.smithy.kotlin.runtime.collections.Attributes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsCredentials
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider
+import software.amazon.awssdk.services.sts.model.Credentials
 
 @Configuration
 class DynamoDbConfig {
@@ -13,7 +19,11 @@ class DynamoDbConfig {
   fun dynamoDbClient(stsAssumeRoleCredentialsProvider: StsAssumeRoleCredentialsProvider): DynamoDbClient {
     return DynamoDbClient {
       region = StsCredentialsProviderConfig.REGION.toString()
-      credentialsProvider = stsAssumeRoleCredentialsProvider as CredentialsProvider
+      credentialsProvider = object : CredentialsProvider {
+        override suspend fun resolve(attributes: Attributes): aws.smithy.kotlin.runtime.auth.awscredentials.Credentials {
+          return stsAssumeRoleCredentialsProvider.resolveCredentials() as aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
+        }
+      }
     }
   }
 }
