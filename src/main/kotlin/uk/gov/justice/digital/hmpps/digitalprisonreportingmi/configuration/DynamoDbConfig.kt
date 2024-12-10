0de5425d7f3bec386cdaba6c94f992f536f8d086
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-12-10 16:38:32 +0000

    DPR2-1514 attempt to fix dynamo client

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/DynamoDbConfig.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
+
+import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
+import aws.smithy.kotlin.runtime.auth.awscredentials.CredentialsProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider
+
+@Configuration
+class DynamoDbConfig {
+
+  @Bean
+  fun dynamoDbClient(stsAssumeRoleCredentialsProvider: StsAssumeRoleCredentialsProvider): DynamoDbClient {
+    return   DynamoDbClient {
+      region = StsCredentialsProviderConfig.REGION.toString()
+      credentialsProvider = stsAssumeRoleCredentialsProvider as CredentialsProvider
+    }
+  }
+}
