5450e6126002abaaeba396b5eb527f43a9eab4e6
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-07-30 16:46:23 +0100

    no_jira - linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/RequestLoggerConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/RequestLoggerConfig.kt
@@ -9,12 +9,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
-class RequestLoggerConfig: Filter {
+class RequestLoggerConfig : Filter {
   private val logger = LoggerFactory.getLogger(RequestLoggerConfig::class.java)
   override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
     val httpRequest = request as HttpServletRequest
     logger.info("Http Request: ${httpRequest.method} ${httpRequest.requestURI}")
     chain?.doFilter(request, response)
   }
-
 }
