b9f681e99afa2d8572051a2d8ce95a66e2a57dfa
James Reed <james.reed1@justice.gov.uk>
2025-10-15 11:46:23 +0100

    add sentry

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/SentryConfig.kt
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
+
+import io.sentry.SentryOptions
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SentryConfig {
+  @Bean
+  fun ignoreHealthRequests() = SentryOptions.BeforeSendTransactionCallback { transaction, _ ->
+    transaction.transaction?.let { if (it.startsWith("GET /health") or it.startsWith("GET /info")) null else transaction }
+  }
+}
