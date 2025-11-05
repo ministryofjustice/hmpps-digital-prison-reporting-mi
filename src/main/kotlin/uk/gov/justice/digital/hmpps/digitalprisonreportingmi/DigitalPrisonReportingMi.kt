4f8bfe96d329c86d00f65f7aa42eb43f68a27cb6
James Reed <james.reed1@justice.gov.uk>
2025-11-05 12:55:10 +0000

    revert blank line change

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DigitalPrisonReportingMi
+
 fun main(args: Array<String>) {
   runApplication<DigitalPrisonReportingMi>(*args)
 }
