2c76aec3fc57e40573d5afbe94e3346c4c59abbb
gavriil-g-moj <135604838+gavriil-g-moj@users.noreply.github.com>
2026-01-30 15:19:48 +0000

    Bump library version to 9.11.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("com.amazon.redshift:redshift-jdbc4-no-awssdk:1.2.45.1069")
-  implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:9.11.15")
+  implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:9.11.17")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.8.2")
 
