16e7433384614dd32b5bacf1ee8e0b87359b0604
madhuri.mahesh <madhuri.mahesh@justice.gov.uk>
2026-03-25 17:18:02 +0000

    DPR2-2520 - exclude SentryAutoConfiguration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,13 +14,14 @@ configurations {
 val testContainersVersion = "1.21.4"
 
 dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("com.amazon.redshift:redshift-jdbc4-no-awssdk:1.2.45.1069")
   implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:13.0.0")
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.2")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.36.0")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.37.0")
 
   // Swagger
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
@@ -3,7 +3,11 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingmi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+  exclude = [
+    io.sentry.spring.boot.jakarta.SentryAutoConfiguration::class,
+  ],
+)
 class DigitalPrisonReportingMi
 
 fun main(args: Array<String>) {
