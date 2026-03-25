9632b833dcccecfb67827092c0e672466005b876
madhuri.mahesh <madhuri.mahesh@justice.gov.uk>
2026-03-25 16:49:36 +0000

    DPR2-2520 - disable web-client for sentry

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/SentryConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/configuration/SentryConfig.kt
@@ -2,12 +2,10 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingmi.configuration
 
 import io.sentry.SentryOptions
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnClass(name = arrayOf("org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration"))
 class SentryConfig {
   @Bean
   fun ignoreHealthRequests() = SentryOptions.BeforeSendTransactionCallback { transaction, _ ->
