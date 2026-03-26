957703db4d73c331a1dde6f6555aa76bb588608d
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2026-03-26 11:53:53 +0000

    Dpr2 2520 sentry incompatibility (#648)
    * DPR2-2520 upgrade sentry
    
    * DPR2-2520 removed webflux as it's causing an issue with sentry
    
    * DPR2-2520 exclude webflux from the library
    
    * DPR2-2520 attempt to exclude only the failing class
    
    * DPR2-2520 fix application.yml
    
    * DPR2-2520 - ConditionalOnClass for org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration
    
    * DPR2-2520 - disable web-client for sentry
    
    * DPR2-2520 - exclude SentryAutoConfiguration
    
    * DPR2-2520 try to use sentry-spring-boot-4
    
    * DPR2-2520 remove old sentry exlcusion
    
    * DPR2-2520 - Upgraded sentry jakarta to 8.37.0
    
    * DPR2-2520 - added tasks.bootJar
    
    * DPR2-2520 - fixed DigitalPrisonReportingMi
    
    * DPR2-2520 - added io.sentry.jvm.gradle
    
    * DPR2-2520 - config back to normal
    
    ---------
    
    Co-authored-by: madhuri.mahesh <madhuri.mahesh@justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
   kotlin("plugin.jpa") version "2.3.10"
   id("jacoco")
   id("org.barfuin.gradle.jacocolog") version "4.0.1"
+  id("io.sentry.jvm.gradle") version "6.2.0"
 }
 
 configurations {
@@ -14,14 +15,12 @@ configurations {
 val testContainersVersion = "1.21.4"
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("io.micrometer:micrometer-registry-prometheus")
   implementation("com.amazon.redshift:redshift-jdbc4-no-awssdk:1.2.45.1069")
   implementation("uk.gov.justice.service.hmpps:hmpps-digital-prison-reporting-lib:13.0.0")
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.2")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.35.0")
 
   // Swagger
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
