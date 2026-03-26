b7cf177e82a9680e2fdd2eabe370c0f820c077a4
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2026-03-26 09:27:48 +0000

    DPR2-2520 remove old sentry exlcusion

--- src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/digitalprisonreportingmi/DigitalPrisonReportingMi.kt
@@ -3,13 +3,9 @@ package uk.gov.justice.digital.hmpps.digitalprisonreportingmi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-  exclude = [
-    io.sentry.spring.boot.jakarta.SentryAutoConfiguration::class,
-  ],
-)
-class DigitalPrisonReportingMi
-
-fun main(args: Array<String>) {
-  runApplication<DigitalPrisonReportingMi>(*args)
+@SpringBootApplication
+class DigitalPrisonReportingMi {
+  fun main(args: Array<String>) {
+    runApplication<DigitalPrisonReportingMi>(*args)
+  }
 }
